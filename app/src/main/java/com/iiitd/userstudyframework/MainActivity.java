8292496d452cdd4b2005e4209e3709ca83d84d60
deepak sood <deepak15013@iiitd.ac.in>
2016-06-12 13:27:28 +0530

    retrived the details from word based password protection application

--- app/src/main/java/com/iiitd/userstudyframework/MainActivity.java
+++ app/src/main/java/com/iiitd/userstudyframework/MainActivity.java
@@ -9,27 +9,31 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String COL_1 = "_id";
-    private static final String COL_2 = "username";
-    private static final String COL_3 = "sign_up_time";
-    private static final String COL_4 = "sign_up_video_location";
-    private static final String COL_5 = "num_of_successful_login";
-    private static final String COL_6 = "mean_login_time";
-    private static final String COL_7 = "video_location_of_every_login";
-    private static final String COL_8 = "num_of_failed_login";
+    private static final String NUM_COL_1 = "_id";
+    private static final String NUM_COL_2 = "username";
+    private static final String NUM_COL_3 = "sign_up_time";
+    private static final String NUM_COL_4 = "sign_up_video_location";
+    private static final String NUM_COL_5 = "num_of_successful_login";
+    private static final String NUM_COL_6 = "mean_login_time";
+    private static final String NUM_COL_7 = "video_location_of_every_login";
+    private static final String NUM_COL_8 = "num_of_failed_login";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState); 
         setContentView(R.layout.activity_main);
 
     }
 
-    public void retrieveDetails(View view) {
-        // Retrieve student records
+    public void retrieveDetailsFromNumDB(View view) {
+
+        Toast.makeText(MainActivity.this, "num db", Toast.LENGTH_SHORT).show();
+        
+        // Retrieve student records from numerical database application
         String URL = "content://com.example.numericalpass.provider/users";
 
         Uri students = Uri.parse(URL);
+
         //Cursor c = managedQuery(students, null, null, null, "");
         // query(Uri,          The content uri
         // mProjection,        The columns to return for each row
@@ -45,14 +49,14 @@ public class MainActivity extends AppCompatActivity {
             if(cursor.moveToFirst()) {
                 do {
                     Toast.makeText(this,
-                            cursor.getString(cursor.getColumnIndex(COL_1)) +
-                                    ", " +  cursor.getString(cursor.getColumnIndex(COL_2)) +
-                                    ", " + cursor.getString(cursor.getColumnIndex(COL_3)) +
-                                    ", " + cursor.getString(cursor.getColumnIndex(COL_4)) +
-                                    ", "+ cursor.getString(cursor.getColumnIndex(COL_5)) +
-                                    ", "+ cursor.getString(cursor.getColumnIndex(COL_6)) +
-                                    ", "+cursor.getString(cursor.getColumnIndex(COL_7)) +
-                                    ", "+cursor.getString(cursor.getColumnIndex(COL_8)),
+                            cursor.getString(cursor.getColumnIndex(NUM_COL_1)) +
+                                    ", " +  cursor.getString(cursor.getColumnIndex(NUM_COL_2)) +
+                                    ", " + cursor.getString(cursor.getColumnIndex(NUM_COL_3)) +
+                                    ", " + cursor.getString(cursor.getColumnIndex(NUM_COL_4)) +
+                                    ", "+ cursor.getString(cursor.getColumnIndex(NUM_COL_5)) +
+                                    ", "+ cursor.getString(cursor.getColumnIndex(NUM_COL_6)) +
+                                    ", "+cursor.getString(cursor.getColumnIndex(NUM_COL_7)) +
+                                    ", "+cursor.getString(cursor.getColumnIndex(NUM_COL_8)),
                             Toast.LENGTH_LONG).show();
                 } while (cursor.moveToNext());
             }
@@ -63,4 +67,47 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+    
+    public void retrieveDetailsFromWordDB(View view) {
+        Toast.makeText(MainActivity.this, "word db", Toast.LENGTH_SHORT).show();
+        // Retrieve student records from numerical database application
+        String URL = "content://com.example.wordpassword.provider/users";
+
+        Uri students = Uri.parse(URL);
+
+        Cursor cursor = null;
+
+        try {
+            cursor = getContentResolver().query(students, null, null, null, "");
+
+            if(cursor.moveToFirst()) {
+                do {
+                    Toast.makeText(this,
+                            cursor.getString(cursor.getColumnIndex(NUM_COL_1)) +
+                                    ", " +  cursor.getString(cursor.getColumnIndex(NUM_COL_2)) +
+                                    ", " + cursor.getString(cursor.getColumnIndex(NUM_COL_3)) +
+                                    ", " + cursor.getString(cursor.getColumnIndex(NUM_COL_4)) +
+                                    ", "+ cursor.getString(cursor.getColumnIndex(NUM_COL_5)) +
+                                    ", "+ cursor.getString(cursor.getColumnIndex(NUM_COL_6)) +
+                                    ", "+cursor.getString(cursor.getColumnIndex(NUM_COL_7)) +
+                                    ", "+cursor.getString(cursor.getColumnIndex(NUM_COL_8)),
+                            Toast.LENGTH_LONG).show();
+                } while (cursor.moveToNext());
+            }
+
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+        
+    }
+    
+    public void retrieveDetailsFromImageDB(View view) {
+        Toast.makeText(MainActivity.this, "image db", Toast.LENGTH_SHORT).show();
+
+
+        
+    }
+    
 }
