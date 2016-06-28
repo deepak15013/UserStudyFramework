061b6b58dfb3439c5ace55c48bedd27e02145246
deepak sood <deepak15013@iiitd.ac.in>
2016-06-28 23:45:23 +0530

    Demographic info updated

--- app/src/main/java/com/iiitd/userstudyframework/MainActivity.java
+++ app/src/main/java/com/iiitd/userstudyframework/MainActivity.java
@@ -21,8 +21,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState); 
         setContentView(R.layout.activity_main);
 
-
-
     }
 
     public void uploadCSVFiles(View view) {
